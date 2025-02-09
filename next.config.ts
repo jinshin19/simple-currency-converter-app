e15b7d7a47f5c5de521d65e3e7335c5afa0cb764
jinshin19 <167668230+jinshin19@users.noreply.github.com>
2025-02-09 17:30:09 +0800

    installed cloudfare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  target: "serverless",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
