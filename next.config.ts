a470c54f7904786d7903fef80e608b1bc154e5da
jinshin19 <167668230+jinshin19@users.noreply.github.com>
2025-02-09 17:44:33 +0800

    added npmrc

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  target: "serverless",
-  reactStrictMode: true,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
